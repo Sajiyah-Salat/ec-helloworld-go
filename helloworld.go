e5253291069551893aa2d8949f9d3f9e6f8f8a70
Chirag Patel <chiragpatel140601@gmail.com>
2023-10-19 14:16:36 +0530

    Update helloworld.go

--- helloworld.go
+++ helloworld.go
@@ -13,7 +13,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if target == "" {
 		target = "Knative"
 	}
-	fmt.Fprintf(w, "Henlo %s!\n", target)
+	fmt.Fprintf(w, "Henlooooooo %s!\n", target)
 }
 
 func main() {
